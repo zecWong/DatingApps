7d43004790d168efe9cec7e1daf96aff3aa8e060
zecWong <2002695A@student.tp.edu.sg>
2022-01-18 13:10:09 +0800

    Add project files.

---
+++ DatingApps/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingApps.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApps.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
